2dea8114f992a2d92a11f691f38f0bb00917337c
ywll <1065575986@qq.com>
2017-09-21 10:41:44 +0800

    save

--- file_io.py
+++ file_io.py
@@ -20,6 +20,7 @@ def findApps(apps, data):
         if len(app_name) != 0:
             apps.append(app_name)
 
+
 def openFile(path):
     f = open(path, "r", encoding='utf-8')
     data = f.read()
@@ -36,17 +37,19 @@ def main():
     a = Counter(apps)
     a = a.most_common()
     # 保存文件
-    file_object = codecs.open(out_path, 'a','utf-8')
+    file_object = codecs.open(out_path, 'a', 'utf-8')
     # 输出结果
     for i in a:
-        str=i.__str__()+"\n"
+        str = i.__str__() + "\n"
         result, number = re.subn(r'[,()\']', '', str)
         file_object.write(result)
         print(result)
     file_object.close()
+
+
 # 配置參數——START
 in_path = "F:\PythonPoj\PythonLearn\正则靶标.txt"  # 文件路径
-out_path="F:\PythonPoj\PythonLearn\\thefile.txt"# 输出路径
+out_path = "F:\PythonPoj\PythonLearn\\thefile.txt"  # 输出路径
 start = 'NOR'  # 头关键字
 end = 'LOC.ADDR'  # 尾关键字
 # 配置蠶食——END

366d193f04f14cd75e2c4821ba83bf4c71671735
miqt <miqingtang@163.com>
2021-12-30 18:27:01 +0800

    去除按照图片拍摄地址分类，因为那样会导致图片更难找

--- ImgClassify.py
+++ ImgClassify.py
@@ -1,91 +1,14 @@
 # !/usr/bin/python
 # -*- coding:utf8 -*-
 import getopt
-import time
-import shutil, os
+import os
+import shutil
 import sys
+import time
+
 import exifread
-import requests
-import json
 
-in_path = ''
-out_path = ''
-
-
-def getLocation(GPSLatitude, GPSLongitude):
-    # http://api.map.baidu.com/reverse_geocoding/v3/?
-    # ak=6oq2afS16woFO4VHB4xmHXGUvyRFrY9G
-    # &output=json
-    # &coordtype=wgs84ll
-    # &location=31.225696563611,121.49884033194
-    if GPSLongitude == '' or GPSLatitude == '':
-        return ''
-
-    du = GPSLatitude.values[0].num  # 度
-    fen = GPSLatitude.values[1].num  # 分
-    miao = GPSLatitude.values[2].num / GPSLatitude.values[2].den  # 秒
-    latitude = (miao / 60.0 + fen) / 60.0 + du
-
-    du = GPSLongitude.values[0].num  # 度
-    fen = GPSLongitude.values[1].num  # 分
-    miao = GPSLongitude.values[2].num / GPSLongitude.values[2].den  # 秒
-    longitude = (miao / 60.0 + fen) / 60.0 + du
-
-    if longitude == 0 or latitude == 0:
-        return ''
-
-    result = requests.get(url="http://api.map.baidu.com/reverse_geocoding/v3/", params={
-        'ak': '6oq2afS16woFO4VHB4xmHXGUvyRFrY9G',
-        'output': 'json',
-        'coordtype': 'wgs84ll',
-        'location': '{latitude},{longitude}'.format(latitude=latitude, longitude=longitude),
-    })
-    if result.status_code == 200:
-        jsonText = json.loads(result.text)
-        statusStr = jsonText.get('status', "")
-        if statusStr == 0:
-            comp = jsonText.get('result', {}).get('addressComponent', {})
-            city = comp.get('city')
-            district = comp.get('district')
-            street = comp.get('street')
-            return city + "/" + district  # + "/" + street
-    return ''
-
-
-def getTimeAndDesc(filePath):
-    f = open(filePath, 'rb')
-    tags = exifread.process_file(f)
-    GPSLatitude = tags.get('GPS GPSLatitude', '')
-    GPSLongitude = tags.get('GPS GPSLongitude', '')
-    make = tags.get('Image Make', None)
-    model = tags.get('Image Model', None)
-
-    try:
-        image_time = getExifTime(tags)
-        if '' != image_time:
-            time = image_time
-        else:
-            time = format_time(os.path.getmtime(filePath))
-    except:
-        time = format_time(os.path.getmtime(filePath))
-        pass
-
-    location = getLocation(GPSLatitude, GPSLongitude)
-
-    if location == '':
-        if (make is not None) and (model is not None):
-            make.values = make.values.strip()
-            model.values = model.values.strip()
-            if make.values == '':
-                return 'other', time
-            if model.values == '':
-                model.values = 'unknow'
-            devName = make.values + "-" + model.values
-            return devName, time
-        else:
-            return 'other', time
-    else:
-        return location, time
+allFileNum = 0
 
 
 def getExifTime(tags):
@@ -105,67 +28,109 @@ def getExifTime(tags):
     return time
 
 
-def format_time(timestamp):
-    time_struct = time.localtime(timestamp)
-    # return time.strftime('%Y-%m-%d %H:%M:%S', time_struct)
-    return time.strftime('%Y-%m-%d', time_struct)
+def printPath(level, path):
+    global allFileNum
+    ''''' 
+    打印一个目录下的所有文件夹和文件 
+    '''
+    # 所有文件夹，第一个字段是次目录的级别
+    dirList = []
+    # 所有文件
+    fileList = []
+    # 返回一个列表，其中包含在目录条目的名称(google翻译)
+    files = os.listdir(path)
+    # 先添加目录级别
+    dirList.append(str(level))
+    for f in files:
+        if (os.path.isdir(path + '/' + f)):
+            # 排除隐藏文件夹。因为隐藏文件夹过多
+            if (f[0] == '.'):
+                pass
+            else:
+                # 添加非隐藏文件夹
+                dirList.append(f)
+        if (os.path.isfile(path + '/' + f)):
+            # 添加文件
+            fileList.append(f)
+            # 当一个标志使用，文件夹列表第一个级别不打印
+    i_dl = 0
+    for dl in dirList:
+        if (i_dl == 0):
+            i_dl = i_dl + 1
+        else:
+            # 打印至控制台，不是第一个的目录
+            print('-' * (int(dirList[0])), dl)
+            # 打印目录下的所有文件夹和文件，目录级别+1
+            printPath((int(dirList[0]) + 1), path + '/' + dl)
+    for fl in fileList:
+        # 打印文件
+        print('-' * (int(dirList[0])), fl)
+        # 随便计算一下有多少个文件
+        allFileNum = allFileNum + 1
 
 
-def find_all_file(fileList, path):
+def get_FileCreateTime(filePath):
+    t = os.path.getmtime(filePath)
+    try:
+        with open(filePath, "rb") as jpegFile:
+            tags = exifread.process_file(jpegFile)
+            image_time = getExifTime(tags)
+            if '' != image_time:
+                return image_time
+    except:
+        pass
+
+    return TimeStampToTime(t)
+
+
+def TimeStampToTime(timestamp):
+    timeStruct = time.localtime(timestamp)
+    # return time.strftime('%Y-%m-%d %H:%M:%S', timeStruct)
+    return time.strftime('%Y-%m-%d', timeStruct)
+
+
+def findFile(fileList, path):
     files = os.listdir(path)
     for f in files:
-        abpath = path + '/' + f
-        if (os.path.isdir(abpath)):
-            find_all_file(fileList, abpath)
-        if (os.path.isfile(abpath)):
-            fileList.append(abpath)
+        if (os.path.isdir(path + '/' + f)):
+            # 排除隐藏文件夹。因为隐藏文件夹过多
+            if (f[0] == '.'):
+                pass
+            else:
+                # 添加非隐藏文件夹
+                findFile(fileList, path + '/' + f)
+        if (os.path.isfile(path + '/' + f)):
+            # 添加文件
+            fileList.append(path + '/' + f)
+
+
+in_path = ''
+out_path = ''
 
 
 def main():
     fileList = []
 
-    find_all_file(fileList, in_path)
+    findFile(fileList, in_path)
+    print(fileList)
 
-    print("开始移动到 -->", in_path)
+    timedir = []
+    print("开始移动" + in_path)
     for img, i in zip(fileList, range(len(fileList))):
-        tag, time = getTimeAndDesc(img)
-        if tag == '':
-            tag = 'other'
-        if time == '':
-            time = "notime"
-        if (img.endswith('mp4')):
-            tag = '视频'
-        if (img.endswith('zip')):
-            tag = 'zip'
-        tag = tag.strip()
-        time = time.strip()
-        timepath = out_path + '/' + tag + "/" + time
-        mkdir(timepath)
+        time = get_FileCreateTime(img)
+        timepath = out_path + '/' + time
+        if time not in timedir:
+            # 新建这个日期的文件夹
+            os.makedirs(timepath, mode=0o777, exist_ok=True)
+            timedir.append(time)
 
         # 将这个文件移动到timedir
-        try:
-            shutil.move(img, timepath)
-            print('[成功]', i + 1, "/", fileList.__len__(), tag, time, img)
-        except:
-            try:
-                mkdir(out_path + '/重复文件/' + tag + "/" + time)
-                shutil.move(img, out_path + '/重复文件/' + tag + "/" + time)
-                print('[重复]', i + 1, "/", fileList.__len__(), tag, time, img)
-            except:
-                print('[错误]', i + 1, "/", fileList.__len__(), tag, time, img)
-                pass
-            pass
+        print(i, "/", fileList.__len__())
+        shutil.move(img, timepath)
     print("完成！")
 
 
-def mkdir(timepath):
-    if not os.path.exists(timepath) or not os.path.isdir(timepath):
-        # 新建文件夹
-        os.makedirs(timepath, mode=0o777, exist_ok=True)
-
-
 if __name__ == '__main__':
-    in_path = out_path = ''
     opts, args = getopt.getopt(sys.argv[1:], 'o:i:', ["help"])
     for opt, arg in opts:
         if opt == '-i':
